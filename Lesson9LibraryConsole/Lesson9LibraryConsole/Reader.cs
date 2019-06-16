a5aea20759092e02a7685e75cb202e50a37eae85
Dmitry <i.endiay@gmail.com>
2019-06-17 00:47:05 +0300

    Create Reader.cs

---
+++ Lesson9LibraryConsole/Lesson9LibraryConsole/Reader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Lesson9LibraryConsole
+{
+    public class Reader
+    {
+        private uint id;
+        string name;
+        string surname;
+        string dateOfBirth;
+        string adress;
+
+        public Reader()
+        {
+            id = 0;
+            name = "no name";
+            surname = "no surname";
+            dateOfBirth = "00/00/0000";
+            adress = "no adress";
+        }
+
+    }
+}
