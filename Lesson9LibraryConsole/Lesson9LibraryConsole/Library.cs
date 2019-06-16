a08946a93b5c7232565c6dac38f796e1e96989a7
Dmitry <i.endiay@gmail.com>
2019-06-17 00:47:07 +0300

    Create Library.cs

---
+++ Lesson9LibraryConsole/Lesson9LibraryConsole/Library.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson9LibraryConsole
+{
+    public class Library
+    {
+        List<Book> books = new List<Book>();
+        List<Reader> readers = new List<Reader>();
+        private Book book;
+        private Reader reader;
+
+        private static uint bookCounter = 0; 
+
+        public static uint BookCounter
+        {
+            get
+            {
+                return bookCounter;
+            }
+
+            set
+            {
+                bookCounter = value;
+            }
+        }
+
+        public Library()
+        {
+
+        }
+
+        public void AddBook()
+        {
+            book = new Book();
+            bookCounter++;
+            book.InputBookData();
+            books.Add(book);
+        }
+
+        public void ChangeBookInformation()
+        {
+
+        }
+
+        public void DeleteBook()
+        {
+
+        }
+
+        public void ShowBooks()
+        {
+            foreach(object bookElement in books)
+            {
+                //Console.WriteLine(bookElement.);
+            }
+        }
+
+        public void AddReader()
+        {
+
+        }
+
+        public void ChangeReaderInformation()
+        {
+
+        }
+
+        public void DeleteReader()
+        {
+
+        }
+
+        public void ShowReaders()
+        {
+
+        }
+
+        public void BookStartRenting()
+        {
+
+        }
+
+        public void BookReturn()
+        {
+
+        }
+
+    }
+}
