b92a62b31b2cfbfeb120cc55c2a8d3ba11374f11
Dmitry <i.endiay@gmail.com>
2019-06-18 00:20:12 +0300

    Update Library.cs

--- Lesson9LibraryConsole/Lesson9LibraryConsole/Library.cs
+++ Lesson9LibraryConsole/Lesson9LibraryConsole/Library.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+
 
 namespace Lesson9LibraryConsole
 {
@@ -10,7 +12,9 @@ namespace Lesson9LibraryConsole
         private Book book;
         private Reader reader;
 
-        private static uint bookCounter = 0; 
+        private static uint bookCounter = 0;
+
+        private string filePath = @"library.txt";
 
         public static uint BookCounter
         {
@@ -34,25 +38,56 @@ namespace Lesson9LibraryConsole
         {
             book = new Book();
             bookCounter++;
+
+            //book class method
             book.InputBookData();
             books.Add(book);
+
+            using (StreamWriter sw = new StreamWriter(filePath, false, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(book.ID + ";" + book.Name + ";" + book.Author + ";" + book.YearOfPublishing + ";");
+            }
         }
 
         public void ChangeBookInformation()
         {
+            Console.WriteLine("Input book ID:");
+            uint bookID = Convert.ToUInt32(Console.ReadLine());
+
+            int index = books.IndexOf(books.Find(item => item.ID == bookID));
+            Console.WriteLine("Book information for changing:");
+            Console.WriteLine("ID: " + books[index].ID + " Author: " + books[index].Author + " Name: " +
+                    books[index].Name + " Year: " + books[index].YearOfPublishing);
+
+            Console.WriteLine("Input the book name:");
+            books[index].Name = Console.ReadLine();
+
+            Console.WriteLine("Input the book author:");
+            books[index].Author = Console.ReadLine();
+
+            Console.WriteLine("Input the year of book publishing:");
+            books[index].YearOfPublishing = Console.ReadLine();
 
         }
 
         public void DeleteBook()
         {
+            Console.WriteLine("Input book ID:");
+            uint bookID = Convert.ToUInt32(Console.ReadLine());
 
+            int index = books.IndexOf(books.Find(item => item.ID == bookID));
+            Console.WriteLine("Deleted book:");
+            Console.WriteLine("ID: " + books[index].ID + " Author: " + books[index].Author + " Name: " +
+                    books[index].Name + " Year: " + books[index].YearOfPublishing);
         }
 
         public void ShowBooks()
         {
-            foreach(object bookElement in books)
+            foreach(Book bookElement in books)
             {
-                //Console.WriteLine(bookElement.);
+                Console.WriteLine("ID: " + bookElement.ID + " Author: " + bookElement.Author + " Name: " + 
+                    bookElement.Name + " Year: " + bookElement.YearOfPublishing);
+               
             }
         }
 
