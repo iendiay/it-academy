5be80869a8ad26f3816710387a409138e067e8f2
Dmitry <i.endiay@gmail.com>
2019-06-18 00:20:14 +0300

    Update Program.cs

--- Lesson9LibraryConsole/Lesson9LibraryConsole/Program.cs
+++ Lesson9LibraryConsole/Lesson9LibraryConsole/Program.cs
@@ -7,10 +7,43 @@ namespace Lesson9LibraryConsole
         static void Main(string[] args)
         {
             Library library = new Library();
-            library.AddBook();
-            library.ShowBooks();
-            library.ChangeBookInformation();
-            library.DeleteBook();
+
+            Console.WriteLine("Commands:");
+            Console.WriteLine("addb - add a book");
+            Console.WriteLine("deleteb - delete a book" );
+            Console.WriteLine("change - change book information");
+            Console.WriteLine("showb - show books");
+            Console.WriteLine("quit - exit the program");
+
+            do
+            {
+                Console.WriteLine("Input command:");
+                switch (Console.ReadLine())
+                {
+                    case "addb":
+                        library.AddBook();
+                        break;
+                    case "deleteb":
+                        library.DeleteBook();
+                        break;
+                    case "changeb":
+                        library.ChangeBookInformation();
+                        break;
+                    case "showb":
+                        library.ShowBooks();
+                        break;
+                    case "quit":
+                        return;
+                }
+            }
+            while (true);
+
+            //library.AddBook();
+            //library.AddBook();
+            //library.ShowBooks();
+            //library.ChangeBookInformation();
+            //library.DeleteBook();
+
         }
     }
 }
