d713fc133fc6b0288154fc846f01254593f8ceea
Dmitry <i.endiay@gmail.com>
2019-06-18 00:20:08 +0300

    Update Book.cs

--- Lesson9LibraryConsole/Lesson9LibraryConsole/Book.cs
+++ Lesson9LibraryConsole/Lesson9LibraryConsole/Book.cs
@@ -10,31 +10,80 @@ namespace Lesson9LibraryConsole
         private string yearOfPublishing;
         DateTime dateOfRenting;
         private bool expired = false;
-        private uint readerID;
+        private uint readerID = 0;
+
+        public uint ID 
+        {
+            get
+            {
+                return id;
+            } 
+
+            set
+            {
+                id = value;
+            } 
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+
+            set
+            {
+                author = value;
+            }
+        }
+
+        public string YearOfPublishing
+        {
+            get
+            {
+                return yearOfPublishing;
+            }
+
+            set
+            {
+                yearOfPublishing = value;
+            }
+        }
+
+
+        public uint ReaderID
+        {
+            get
+            {
+                return readerID;
+            }
+
+            set
+            {
+                readerID = value;
+            }
+        }
 
         public Book()
         {
 
         }
 
-        public uint ID
-        {
-            get
-            {
-                return id;
-            }
 
-            set
-            {
-                id = value;
-            }
-        }
-        /*
-        public uint GetID()
-        {
-            return id;
-        }
-        */
         public void InputBookData()
         {
             Console.WriteLine("Input the book name:");
@@ -46,15 +95,21 @@ namespace Lesson9LibraryConsole
             Console.WriteLine("Input the year of book publishing:");
             yearOfPublishing = Console.ReadLine();
 
-            dateOfRenting = DateTime.Today;
+            //dateOfRenting = DateTime.Today;
 
             id = Library.BookCounter;
 
+            /*
             Console.WriteLine("Input reader ID:");
             readerID = Convert.ToUInt32(Console.ReadLine());
-
+            */
         }
 
+        /*
+        public void SearchBook(int id)
+        {
 
+        }
+        */
     }
 }
