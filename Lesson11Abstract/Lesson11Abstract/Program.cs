de60414865338b0264e8959c574986f1c775cd1e
Dmitry <i.endiay@gmail.com>
2019-06-20 00:07:27 +0300

    Create Program.cs

---
+++ Lesson11Abstract/Lesson11Abstract/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Lesson11Abstract
+{
+    abstract class ATM
+    {
+        private string adress;
+
+        public string Adress
+        {
+            get
+            {
+                return adress;
+            }
+
+            set
+            {
+                adress = value;
+            }
+        }
+
+        public decimal getCash(decimal cash)
+        {
+            Console.WriteLine("Your cash: " + cash);
+            return cash;
+        } 
+
+
+
+    }
+
+    abstract class ATMPriorbank : ATM
+    {
+        public decimal getBonus(decimal cash)
+        {
+            decimal bonusCoefficient = 0.01M;
+            decimal cashBonus = cash * bonusCoefficient;
+            Console.WriteLine("Your bonus: " + cashBonus);
+            return cashBonus;
+        }
+    }
+
+    class ATMPrior : ATMPriorbank
+    {
+        
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ATMPrior atmP = new ATMPrior();
+
+            atmP.Adress = "prospekt Masherova 40";
+
+            Console.WriteLine("Input cash value:");
+            decimal cash = Convert.ToDecimal(Console.ReadLine());
+
+            atmP.getCash(cash);
+            atmP.getBonus(cash);
+            Console.WriteLine(atmP.Adress);
+
+            Console.ReadKey();
+        }
+    }
+}
