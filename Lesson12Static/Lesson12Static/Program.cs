9cfb52a2344b36dd8ee7ea1879c6b84c9c4c0dc3
Dmitry <i.endiay@gmail.com>
2019-06-24 08:06:43 +0300

    Create Program.cs

---
+++ Lesson12Static/Lesson12Static/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class ApplicationSettings
+{
+    public static string BaseDir;
+    public static string GetRelativeDir() 
+    {
+        return "q"; 
+    }
+}
+
+static class SimpleClass
+{
+    static readonly long baseline;
+
+    static SimpleClass()
+    {
+        baseline = DateTime.Now.Ticks;
+    }
+
+    public static long getBaseline()
+    {
+        return baseline;
+    }
+
+
+}
+
+namespace Lesson12Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ApplicationSettings as = new ApplicationSettings();
+
+            Console.WriteLine(ApplicationSettings.GetRelativeDir());
+
+            //SimpleClass sc = new SimpleClass();
+
+            Console.WriteLine(SimpleClass.getBaseline());
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
