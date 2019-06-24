6e039bb9d862041bbbfd57079b3a2b8fda944695
Dmitry <i.endiay@gmail.com>
2019-06-24 08:06:20 +0300

    Create Client.cs

---
+++ Lesson12Bank/Lesson12Bank/Client.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace Lesson12Bank
+{
+    public class Client : Person
+    {
+        private uint _id;
+        private uint _numberOfCards;
+
+        public Client()
+        {
+            _id = 0;
+            _numberOfCards = 0;
+        }
+
+        public uint ID
+        {
+            get
+            {
+                return _id;
+            }
+
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public uint NumberOfCards
+        {
+            get
+            {
+                return _numberOfCards;
+            }
+
+            set
+            {
+                _numberOfCards = value;
+            }
+        }
+
+
+        public void ShowAccountInfo()
+        {
+
+        }
+
+
+    }
+}
