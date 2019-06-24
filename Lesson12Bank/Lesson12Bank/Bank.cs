3b4252085051baa039eac6224b45bf73d5b5d577
Dmitry <i.endiay@gmail.com>
2019-06-24 08:06:12 +0300

    Create Bank.cs

---
+++ Lesson12Bank/Lesson12Bank/Bank.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson12Bank
+{
+    public class Bank
+    {
+        List<Client> clients = new List<Client>();
+        List<Card> cards = new List<Card>();
+
+        private Client _client;
+        private Card _card;
+
+        private const string filePathClients = @"Clients.txt";
+        private const string filePathCards = @"Cards.txt";
+
+        public Bank()
+        {
+
+        }
+
+        public void AddClient()
+        {
+            _client = new Client();
+            //clientsCounter++;
+
+            /*
+            book.InputBookData();
+            books.Add(book);
+
+            using (StreamWriter sw = new StreamWriter(filePath, false, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(book.ID + ";" + book.Name + ";" + book.Author + ";" + book.YearOfPublishing + ";");
+            }
+            */
+        }
+
+        public void DeleteClient()
+        {
+
+        }
+
+        public void ChangeClientInformation()
+        {
+
+        }
+
+        public void ShowClients()
+        {
+
+        }
+
+        public void AddCard()
+        {
+
+        }
+
+        public void DeleteCard()
+        {
+
+        }
+
+        public void ShowCards()
+        {
+
+        }
+
+    }
+}
