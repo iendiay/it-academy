13644d475bb52c57f939597864a7a16b8e8bcc88
Dmitry <i.endiay@gmail.com>
2019-06-24 08:06:28 +0300

    Create Program.cs

---
+++ Lesson12Bank/Lesson12Bank/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Lesson12Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+
+            Console.WriteLine("Commands:");
+            Console.WriteLine("addcl - add a client");
+            Console.WriteLine("deletecl - delete a client");
+            Console.WriteLine("changecl - change client information");
+            Console.WriteLine("showcl - show clients");
+            Console.WriteLine("addcd - add a card");
+            Console.WriteLine("deletecd - delete a card");
+            Console.WriteLine("showcd - show cards");
+            Console.WriteLine("exit - exit the program");
+
+            do
+            {
+                Console.WriteLine("\nInput command:");
+                switch (Console.ReadLine())
+                {
+                    case "addcl":
+                        bank.AddClient();
+                        break;
+                    case "deletecl":
+                        bank.DeleteClient();
+                        break;
+                    case "changecl":
+                        bank.ChangeClientInformation();
+                        break;
+                    case "showcl":
+                        bank.ShowClients();
+                        break;
+                    case "addcd":
+                        bank.AddCard();
+                        break;
+                    case "deletecd":
+                        bank.DeleteCard();
+                        break;
+                    case "showcd":
+                        bank.ShowCards();
+                        break;
+                    case "exit":
+                        return;
+                }
+            }
+            while (true);
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
