b5b650e05f4f6e1e814a410d89ffb0c9dad628a3
Dmitry <i.endiay@gmail.com>
2019-06-24 08:06:25 +0300

    Create Person.cs

---
+++ Lesson12Bank/Lesson12Bank/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace Lesson12Bank
+{
+    public abstract class Person
+    {
+
+        private string _name;
+        private string _adress;
+        private string _phone;
+        private string _email;
+
+        public Person()
+        {
+
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string Adress
+        {
+            get
+            {
+                return _adress;
+            }
+
+            set
+            {
+                _adress = value;
+            }
+        }
+
+        public string Phone
+        {
+            get
+            {
+                return _phone;
+            }
+
+            set
+            {
+                _phone = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+
+            set
+            {
+                _email = value;
+            }
+        }
+    }
+}
