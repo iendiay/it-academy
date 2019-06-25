d1ea083d7b2b8cb47eab655815e6e3d01d8920c5
Dmitry <i.endiay@gmail.com>
2019-06-25 19:26:17 +0300

    Create Program.cs

---
+++ Lesson14Delegate/Lesson14Delegate/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Lesson14Delegate
+{
+    class Program
+    {
+        delegate int Transformer(int x);
+
+        /////////
+        delegate void Delegate1();
+        delegate void Delegate2();
+
+       
+
+        static void Main(string[] args)
+        {
+            Transformer t = Square;
+            Transformer t1 = Cube;
+
+            Console.WriteLine(t.GetHashCode());
+            Console.WriteLine(t1.GetHashCode());
+
+            t += Square;
+            t += Cube;
+            Console.WriteLine(t.GetHashCode());
+
+            t += Square;
+            Console.WriteLine(t.GetHashCode());
+
+            int result = t(3);
+            //Console.WriteLine(result);
+
+            t -= Square;
+            Console.WriteLine(t.GetHashCode());
+
+            result = t(3);
+            //Console.WriteLine(result);
+
+            /////////////////////////
+            Delegate1 del1 = new Delegate1(Method);
+            Delegate2 del2 = new Delegate2(Method);
+
+            //int result = del2;
+            del2 = del1.Invoke;
+            Delegate2 del3 = new Delegate2(del1);
+            Delegate del = new Delegate1(Method);
+            Console.WriteLine(del.GetType().BaseType.Name);
+        }
+
+        static int Square(int x)
+        {
+            Console.WriteLine(x*x);
+            return x * x;
+        }
+
+        static int Cube(int x)
+        {
+            Console.WriteLine(x*x*x);
+            return x * x * x;
+        }
+
+        /////////////
+        static void Method()
+        {
+            Console.WriteLine("/nMethod");
+        }
+    }
+}
