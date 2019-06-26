d94967595b3f2dde7894e169a30a5b747700bacf
Dmitry <i.endiay@gmail.com>
2019-06-27 02:14:55 +0300

    Create Program.cs

---
+++ Lesson14Roulette/Lesson14Roulette/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Lesson14Roulette
+{
+    class Program
+    {
+        class Roulette
+        {
+            public delegate void MethodContainer(string message);
+            public event MethodContainer GetNumber;
+
+            public void Spin()
+            {
+                Random rand = new Random();
+                int number = rand.Next(0, 36);
+
+                if (GetNumber != null)
+                    GetNumber($"Number: {number}");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+
+            Roulette roulette = new Roulette();
+            roulette.GetNumber += ShowMessage;
+
+            ConsoleKeyInfo k;
+
+            do
+            {
+                Console.WriteLine("Please spin the roulette by spacebar.");
+
+                k = Console.ReadKey(true);
+
+                if (k.Key == ConsoleKey.Spacebar)
+                {
+                    roulette.Spin();
+                }   
+            }
+            while (k.Key != ConsoleKey.Escape);
+
+            Console.ReadKey();
+
+        }
+
+        private static void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+        
+    }
+}
