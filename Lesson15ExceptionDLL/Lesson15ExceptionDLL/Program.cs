62b4fba000db574844c38a8a2ded97264419c4f7
Dmitry <i.endiay@gmail.com>
2019-06-30 23:15:41 +0300

    Create Program.cs

---
+++ Lesson15ExceptionDLL/Lesson15ExceptionDLL/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Lesson15ExceptionDLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            do
+            {
+                Console.WriteLine("Input command:");
+                switch (Console.ReadLine())
+                {
+                    case "+":
+                        calc.Addition();
+                        break;
+                    case "-":
+                        calc.Subtraction();
+                        break;
+                    case "*":
+                        calc.Multiplication();
+                        break;
+                    case "/":
+                        try
+                        {
+                            calc.Division();
+                        }
+                        catch (DivideByZeroException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }                           }
+                        catch(ValueException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+
+                    case "exit":
+                        return;
+                }
+            }
+            while (true);
+
+
+
+
+        }
+    }
+}
