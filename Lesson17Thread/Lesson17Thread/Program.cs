7df6eebae601a76a687cdadce9ca9d9a006de1e2
Dmitry <i.endiay@gmail.com>
2019-07-02 17:55:55 +0300

    Create Program.cs

---
+++ Lesson17Thread/Lesson17Thread/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Lesson17Thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Start();
+            }
+        }
+
+        private static void Func()
+        {
+            Console.WriteLine("Func{0}", Thread.CurrentThread.GetHashCode());
+        }
+
+        public static void Start()
+        {
+            Console.WriteLine("Main(){0}", Thread.CurrentThread.GetHashCode());
+            ThreadStart del = new ThreadStart(Func);
+            Thread th = new Thread(del);
+            
+            th.Start();
+            
+        }
+    }
+}
